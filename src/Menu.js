7130afe276b67bed5eae0a9a6f5e66acd8c45a1a
Rachel Hovell <r.hovell@live.com>
2018-11-03 21:26:30 +0000

    Fixed import issue

--- src/Menu.js
+++ src/Menu.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Search from './Search'
-import Park from './parkData'
-
-
 
 const Menu = ({ parks }) => {
 
