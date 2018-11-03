45c0c8af05e1a9dc4a667d3fbd6b7f8fa456e4c9
Rachel Hovell <r.hovell@live.com>
2018-11-03 21:18:13 +0000

    Added park state to main app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import './App.css'
 import Main from './Main'
+import * as Parks from './parks'
 
 class App extends Component {
-
+  state = {
+    parks: []
+  }
+  componentDidMount() {
+  Parks.getAll().then((parks) => {
+    this.setState({ parks : parks.results.items })
+    console.log(this.state.parks)
+  })
+}
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <Main/>
+          <Main
+          parks={this.state.parks}/>
         )}/>
         </div>
     )
