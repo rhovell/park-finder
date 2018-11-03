26cbb7ba2f8012a1e15b199fed64cdd4ad97b599
Rachel Hovell <r.hovell@live.com>
2018-11-03 21:17:24 +0000

    Iterated over api results to create markers

--- src/map.js
+++ src/map.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import Parks from './parks';
-import * as parkData from './parkData'
+import * as Parks from './parks'
 
 export class MapContainer extends Component {
   state = {
@@ -16,7 +15,7 @@ export class MapContainer extends Component {
       activeMarker: marker,
       showingInfoWindow: true
     });
-    this.componentDidMount();
+
     console.log(this.state.selectedPlace)
   };
 
@@ -27,6 +26,13 @@ export class MapContainer extends Component {
         activeMarker: null
       })
     }
+  };
+
+  getPosition = (position) => {
+      var iterator = position.values();
+      let lat = iterator.next().value;
+      let lang = iterator.next().value;
+      return {lat: lat ,lng: lang};
   };
 
   windowHasOpened = (props) => {
@@ -47,53 +53,34 @@ export class MapContainer extends Component {
           zoom={12}
           onClick={this.onMapClicked}
           visible={true}
+          parks={this.props.parks}
           >
 
-
-          <Marker
-            title={'Alexandra Park'}
-            name={'Alexandra Park'}
-            position={{lat: 53.45094, lng: -2.24502}}
-            info={[]}
-            onClick={this.onMarkerClick} />
-          <Marker
-            title={'Heaton Moor Park'}
-            name={'Heaton Moor Park'}
-            position={{lat: 53.425912, lng: -2.185507}}
-            onClick={this.onMarkerClick} />
-          <Marker
-            name={'Mersey Vale Nature Park'}
-            title={'Mersey Vale Nature Park'}
-            address={'Stockport, SK4 3EA'}
-            onClick={this.onMarkerClick} />
-          <Marker
-            name={'Abney Hall Park'}
-            title={'Abney Hall Park'}
-            position={{lat: 53.398543, lng: -2.21045}}
-            onClick={this.onMarkerClick} />
-          <Marker
-            name={'Reddish Vale Country Park'}
-            title={'Reddish Vale Country Park'}
-            position={{lat: 53.437132, lng: -2.146727}}
-            onClick={this.onMarkerClick} />
-          <Marker
-            name={'Woodbank Park'}
-            title={'Woodbank Park'}
-            position={{lat: 53.410599, lng: -2.141143}}
-            onClick={this.onMarkerClick} />
-          <Marker />
-
+          {this.props.parks.map(park => (
+            <Marker
+            key={park.id}
+            title={park.title}
+            position={this.getPosition(park.position)}
+            onClick={this.onMarkerClick}
+            />
+          ))
+        }
+        {this.props.parks.map(park => (
+          <div key={park.id}
+          className="pop-up">
           <InfoWindow
+
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
             onOpen={this.windowHasOpened}
-            onClose={this.windowHasClosed}>
-              <div>
-                <h3>{this.state.selectedPlace.name}</h3>
-                <div className="address">{this.state.selectedPlace.address}</div>
-              </div>
-          </InfoWindow>
+            onClose={this.windowHasClosed}
+            title={park.title}
+          />
 
+          <div className="info-title">{park.title}</div>
+          </div>
+          ))
+        }
       </Map>
     );
   }
