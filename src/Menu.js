fa1dad415e013e4961a74f189206d10c62cfbe7c
Rachel Hovell <r.hovell@live.com>
2018-11-04 18:45:56 +0000

    Restructed Menu to state controlled with menu button

--- src/Menu.js
+++ src/Menu.js
@@ -2,35 +2,70 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Search from './Search'
+import * as Parks from './parks'
 
-const Menu = ({ parks }) => {
 
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.showMenu = this.showMenu.bind(this);
+    this.hideMenu = this.hideMenu.bind(this);
+    this.state = { hidden: true };
+  }
+
+    showMenu = () => {
+        this.setState({ hidden: false })
+    };
+
+    hideMenu = () => {
+        this.setState({ hidden: true })
+    };
+
+    showData = () => {
+
+    }
+
+  render() {
+    const menuHidden = this.state.hidden;
+    let menu;
+    let button;
+
+    if (menuHidden) {
+      button = <ShowButton onClick={this.showMenu} >Menu</ShowButton>;
+      menu;
+    } else {
+      button = <HideButton onClick={this.hideMenu} >Menu</HideButton>;
+      menu =
+      <div className="menu open">
+      <div className="search-container">
+        <Search
+        parks={this.props.parks}/>
+      </div>
+      </div>;
+    };
 
     return (
-    <div className="menu-container">
-      <div className="search-container">
-        <Search />
-      </div>
-      <div className="park-container">
-        <ul className="park-list">
-        {parks.map(park => (
-            <li key={park.id}>
-            <div className="park-title">{park.title}</div>
-            <div className="park-data">
-            <div className="park-address">{park.vicinity}</div>
-
-
-            </div>
-            </li>
-            ))
-          }
-        </ul>
-      </div>
+      <div className="menu-container">
+      <div className="menu-button">{button}</div>
+      {menu}
     </div>
   )
 }
-Menu.propTypes = {
-parks: PropTypes.array.isRequired,
+}
+
+function ShowButton(props) {
+return (
+  <button onClick={props.onClick}>
+    Show Menu
+  </button>
+);
 };
 
+function HideButton(props) {
+return (
+  <button onClick={props.onClick}>
+    Hide Menu
+  </button>
+);
+};
 export default Menu
