9d3614d6bf0c79da3b4c724b9d0cfbad4424d200
Rachel Hovell <r.hovell@live.com>
2018-11-04 18:47:58 +0000

    Added debounce, restructured to state controlled with results in menu

--- src/Search.js
+++ src/Search.js
@@ -1,39 +1,67 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import * as Parks from './parks'
+import debounce from 'lodash.debounce'
 
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 'Search...' };
+  state = {
+      searchResults: []
+  }
+  componentWillMount = () => {
+     this.delayedCallback = debounce(function (event) {
+       this.updateResults(event.target.value);
+     }, 1000);
   }
 
-  clear = () => {
-    this.setState({ value: '' })
+  onChange = (event) => {
+    event.persist();
+    this.delayedCallback(event);
   }
-
-  search = (event) => {
-    this.setState({ value : event.target.value })
+  updateResults = (query) => {
+    Parks.get(query).then((searchQuery) => {
+      this.setState({ searchResults : searchQuery.results.items })
+    }).then(() => {
+      for(var result of this.state.searchResults){
+        function isPark(result) {
+          return result.category.id === 'recreation';
+        }
+        var filtered = this.state.searchResults.filter(isPark);
+      }
+        this.setState({ searchResults: filtered })
+        console.log(this.state.searchResults)
+    }).catch((error) => {
+        this.setState({ searchResults : [] })
+        console.log('Error on search request')
+        })
   }
-
   render() {
-
-
+    const searchResults = this.state;
     return (
-    <input type="text"
-      value={this.state.value}
-      onChange={this.search}
-      onClick={this.clear}
+      <div className="search-parks-container">
+      <input
+        onChange={(event) => this.onChange(event)}
+        placeholder="Search..."
+        type="search"
       />
+
+      <div className="search-park-results">
+          <ul className="park-list">
+          {this.state.searchResults.length > 0  ?
+            this.state.searchResults.map((searchResult) => (
+              <li key={[searchResult.id, 'Menu-item']}
+              title={searchResult.title}
+              >
+              {searchResult.title}
+              </li>
+          )) : <li className="no-results">No Results Found.</li>}
+          </ul>
+        </div>
+  </div>
 
   );
   }
 
 };
 
-
-ReactDOM.render(
-  <Search />,
-  document.getElementById('root')
-);
 export default Search
