9f6a66cab55f0654282dab3129e93f78a5dcd4e2
Rachel Hovell <r.hovell@live.com>
2018-11-04 18:50:42 +0000

    Added search api parameter

--- src/parks.js
+++ src/parks.js
@@ -9,28 +9,24 @@ const lang = '&Accept-Language=en%2Cen-US%3Bq%3D0.5'
 const appId = '&app_id=9XpHIanLjpmAG8iNneCJ';
 const appCode = '&app_code=VJo1BnHvW0qNp4i_OH5rMw&pretty';
 
+// https://places.cit.api.here.com/places/v1/discover/explore?at=53.410632%2C-2.157533&cat=natural-geographical&drilldown=true&Accept-Language=en%2Cen-US%3Bq%3D0.5&app_id=9XpHIanLjpmAG8iNneCJ&app_code=VJo1BnHvW0qNp4i_OH5rMw
+
 const headers = {
  'Accept-Encoding': 'gzip',
  'Accept-Language': 'en,en-US;q=0.5',
  'Accept': 'application/json'
 }
+const apiSearch = 'https://places.cit.api.here.com/places/v1/discover/search';
+const queryStart = '&q='
 
-export const get = (parkId) =>
-  fetch(`${api}${position}${category}${filter}${type}${content}${lang}${appId}${appCode}`, { headers })
+export const get = (query) =>
+  fetch(`${apiSearch}${position}${queryStart}`+query+`${lang}${appId}${appCode}`, { headers })
   .then(function(response) {
     return response.json();
   });
-
+// https://places.cit.api.here.com/places/v1/discover/search?at=53.410632%2C-2.157533&q=fog&Accept-Language=en%2Cen-US%3Bq%3D0.5&app_id=9XpHIanLjpmAG8iNneCJ&app_code=VJo1BnHvW0qNp4i_OH5rMw
 export const getAll = () =>
   fetch(`${api}${position}${category}${filter}${type}${content}${lang}${appId}${appCode}`, { headers })
     .then(function(response) {
       return response.json();
     });
-
-export const search = (query) =>
-  fetch(`${api}${position}${category}${filter}${type}${content}${lang}${appId}${appCode}/search`, {
-    method: 'POST',
-    body: JSON.stringify({ query })
-  }).then(function(response) {
-        return response.json();
-      });
